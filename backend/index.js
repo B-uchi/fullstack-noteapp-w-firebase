376ec84f866c44e0a5d20c02671b27c854cbb32b
Ibuchukwu Umeano <ibumeano@gmail.com>
2023-09-07 12:05:31 +0100

    env variables

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { PORT, MongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { createUser, loginUser } from "./user/createandlogin.js";
 import { createNote } from "./user/createNote.js";
@@ -21,24 +20,16 @@ app.post("/login", loginUser);
 app.post("/notes", createNote);
 app.get('/notes', fetchNotes)
 
-// app.get('/notes', async (request, response)=>{
-//     try {
-//         const notes = await Note.find({});
-//         return response.status(200).json(notes)
-//     } catch (error) {
-//         console.log(error.message)
-//     }
-// })
 
 mongoose
-  .connect(MongoDBURL, {
+  .connect(process.env.MongoDBURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Database connected Sussessfully");
-    app.listen(PORT, () => {
-      console.log(`Server listening on ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on ${process.env.PORT}`);
     });
   })
   .catch((e) => {
