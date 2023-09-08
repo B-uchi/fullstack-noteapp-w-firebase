65a2d5bc038d7202d63e706a4005a30475312146
Buchi Umeano <108285540+B-uchi@users.noreply.github.com>
2023-09-08 08:05:33 +0100

    Update config.cjs

--- backend/firebase/config.cjs
+++ backend/firebase/config.cjs
@@ -1,8 +1,19 @@
-const credentials = require('./credentialss.cjs').default
 const  firebase = require("firebase-admin")
 
 const firebaseApp = firebase.initializeApp({
-  credential: firebase.credential.cert(credentials),
+  credential: firebase.credential.cert({
+  "type": "service_account",
+  "project_id": "mern-note-server",
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.client_x509_cert_url,
+  "universe_domain": "googleapis.com"
+}),
 });
 
 
